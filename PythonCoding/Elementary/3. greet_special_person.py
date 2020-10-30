8374df45909ff3401fb28c1aa1c83e1dd1664c73
supriem <suprim.regmi.5@gmail.com>
2020-10-30 18:12:33 +0545

    Added in patch mode

--- PythonCoding/Elementary/3. greet_special_person.py
+++
@@ -1,6 +0,0 @@
-# Modify the previous program such that only the users Alice and Bob are greeted with their names.
-
-name = input("Enter your name: ")
-if name == "Alice" or name == "Bob":
-    print("Hello", name)
-
